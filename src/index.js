c135f402fc0c847ea7ffeeac32af499b8ab16ef2
taegen-seto <taegenseto@gmail.com>
2023-12-17 20:53:16 +1000

    updated user experience and styling

--- src/index.js
+++ src/index.js
@@ -13,10 +13,12 @@ function generateRecommendation(event) {
   let apiKey = "384310c338bfe3o7f06caedc66df497t";
   let prompt = `User Request: please recommend a book similar to but not the same as ${userRequest.value}`;
   let context =
-    "You are an expert in books and you follow book trends on social media and goodreads. You love to make clear and concise book recommendations. Please recommend a book to read based on the user instructions. Please do not include reasoning for your decision. Please do not include a heading, only provide a book title and its author";
+    "You are an expert in books, genres and authors and you follow current book trends on social media and goodreads. You love to make clear and concise book recommendations. Please recommend a book to read based on the user instructions. Please do not include reasoning for your decision. Please do not include a heading or title, only provide a book title and its author";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let recommendationFormElement = document.querySelector("#recommendation");
+  let recommendationElement = document.querySelector("#recommendation");
+  recommendationElement.classList.remove("hidden");
+  recommendationElement.innerHTML = `<div class="generating">⏳ Finding you a book recommendation based on <em>${userRequest.value}</em></div>`;
 
   axios.get(apiUrl).then(displayRecommendation);
 }
